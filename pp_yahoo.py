eef9c2f73969e9429159a2c8cc1191fcba1905ea
Dean Ting <freehotday@gmail.com>
2015-07-15 17:23:39 +0800

    add function parse_url_bfs

--- pp_yahoo.py
+++ pp_yahoo.py
@@ -1,4 +1,36 @@
 import urllib, re, os
+from bs4 import BeautifulSoup
+
+visited = []
+next_layer = []
+
+def parse_url_bfs(html, layer = 1):
+	"""
+	parse url with bfs
+
+	params html : root url
+	params layer: visit layer
+
+	"""
+	cur_layer_n = 1
+	cur_layer = []
+	cur_layer.append(html)
+	next_layer = []
+	while (cur_layer_n <= layer):
+		for link in cur_layer:
+			page = urllib.urlopen(link)
+			soup = BeautifulSoup(page.read(),"lxml")
+			for a_line in soup.findAll('a', href=True):
+				url = a_line.get("href")
+				if url not in visited:
+					visited.append(url)
+					next_layer.append(url)
+		cur_layer_n+=1
+
+	print visited
+
+
+
 
 
 def parse_prod_name(page):
@@ -14,6 +46,16 @@ def parse_prod_name(page):
 
 
 if __name__ == '__main__':
-	page = urllib.urlopen("https://tw.buy.yahoo.com/?catitemid=26650&hpp=catid6365catitem26650")
-	parse_prod_name(page)
+	#page = urllib.urlopen("https://tw.buy.yahoo.com/?catitemid=26650&hpp=catid6365catitem26650")
+	#page = urllib.urlopen("https://tw.buy.yahoo.com/")
+	#parse_prod_name(page)
 	#print (page.read())
+	#soup = BeautifulSoup(page.read(), "lxml")
+	#print (soup.a)
+	#print (soup.a['class'])
+	#print (soup.findAll('a', href=True)[0].get('href'))
+	#for link in soup.findAll('a', href=True):
+
+		#if "yui3-menu-label" in link['class']:
+		#	print link.get(href)
+	parse_url_bfs("https://tw.buy.yahoo.com/", 1)
