acc9a5803f9be3f7e35d97c06624736aebe55041
Dean Ting <freehotday@gmail.com>
2015-07-16 16:45:59 +0800

    current version has a bug.
    can not parser more than 3 layer

--- pp_yahoo.py
+++ pp_yahoo.py
@@ -1,4 +1,5 @@
 import urllib, re, os
+import urllib.request
 from bs4 import BeautifulSoup
 
 visited = []
@@ -15,56 +16,59 @@ def parse_url_bfs(html, layer = 1):
 	cur_layer_n = 1
 	cur_layer = []
 	cur_layer.append(html)
+	visited.append(html)
 	next_layer = []
 	while (cur_layer_n <= layer):
 		for link in cur_layer:
-			page = urllib.urlopen(link)
-			soup = BeautifulSoup(page.read(),"lxml")
-			for a_line in soup.findAll('a', href=True):
-				url = a_line.get("href")
-				if url not in visited:
-					visited.append(url)
-					next_layer.append(url)
+			if "http" in link:
+				#print (link + "\n")
+				page = urllib.request.urlopen(link)
+				soup = BeautifulSoup(page.read(),"lxml")
+				for a_line in soup.findAll('a', href=True):
+					url = a_line.get("href")
+					if url[0:2] == "/?":
+						cut_pos = link.find("/?sub")
+						url = link[:cut_pos] + url
+						#print (url)
+					if url not in visited:
+						visited.append(url)
+						next_layer.append(url)
+		cur_layer = list(next_layer)
 		cur_layer_n+=1
+	
+	f = open("url_all.txt", "w+")
+	for url in visited:
+		f.write(url+ "\n")
+		#print (url+"\n")
+	f.close()
+	
 
-	print visited
 
 
 
 
-
-def parse_prod_name(page):
+def parse_prod_name(page, f):
 	"""
 	parse product name from page
 	"""
-	"""
-	pd_name = []
-	line = page.readline()
-	while(line != None):
-		if ("<li class=\"yui3-u-1 name\">" in line):
-			print line
-		line = page.readline()
-	"""
 	soup = BeautifulSoup(page.read(),"lxml")
 	for li_line in soup.findAll("li", class_="yui3-u-1 name"):
-		print li_line
+		print (li_line.get_text())
+		f.write(li_line.get_text()+ "\n")
 
 
 if __name__ == '__main__':
-	#page = urllib.urlopen("https://tw.buy.yahoo.com/?catitemid=26650&hpp=catid6365catitem26650")
-	#page = urllib.urlopen("https://tw.buy.yahoo.com/")
-	#parse_prod_name(page)
-	#print (page.read())
-	#soup = BeautifulSoup(page.read(), "lxml")
-	#print (soup.a)
-	#print (soup.a['class'])
-	#print (soup.findAll('a', href=True)[0].get('href'))
-	#for link in soup.findAll('a', href=True):
-
-		#if "yui3-menu-label" in link['class']:
-		#	print link.get(href)
+	parse_url_bfs("https://tw.buy.yahoo.com/", 2)
+	#parse_url_bfs("https://tw.buy.yahoo.com/?sub=1", 1)
 	
-
-	#parse_url_bfs("https://tw.buy.yahoo.com/", 1)
-	page = urllib.urlopen("https://tw.buy.yahoo.com/?catitemid=52982&hpp=sub410catid3955catitem52982")
-	parse_prod_name(page)
+	f = open("yahoo_pd_name.txt", "w+")
+	url_n = 0
+	for url in visited:
+		if "http" in url:
+			url_n += 1
+			print (url+'\n')
+			page = urllib.request.urlopen(url)
+			parse_prod_name(page, f)
+	f.close()
+	print ("url_n : ", url_n)
+	print ("visited size : ", len(visited))
