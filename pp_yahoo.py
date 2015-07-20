f7420bb79d58694528358d4e5ddf9b8ab75c3ba9
Dean Ting <freehotday@gmail.com>
2015-07-20 18:22:41 +0800

    add threadpoool part for parser product name in pp_yahoo.py
    add data file(url, product name)

--- pp_yahoo.py
+++ pp_yahoo.py
@@ -1,5 +1,6 @@
 import urllib, re, os
 import urllib.request
+import concurrent.futures
 from bs4 import BeautifulSoup
 
 visited = []
@@ -54,23 +55,79 @@ def parse_url_bfs(html, layer = 1):
 	print (num)
 	
 
-
-
-
-
 def parse_prod_name(page, f):
 	"""
 	parse product name from page
 	"""
 	soup = BeautifulSoup(page.read(),"lxml")
 	for li_line in soup.findAll("li", class_="yui3-u-1 name"):
-		print (li_line.get_text())
+		#print (li_line.get_text())
 		f.write(li_line.get_text()+ "\n")
+
+def divide_url_all():
+	"""
+	divide file in several file
+	"""
+	f = open("url_all.txt", "r+")
+	url_amount = 0
+	file_num = 1
+	line = f.readline()
+	sub_f = open("url_"+str(file_num)+".txt", "w+")
+	while(line != ""):
+		#print ("line : " + line )
+		url_amount += 1
+		sub_f.write(line)
+		if url_amount > 33999:
+			sub_f.close()
+			url_amount = 0
+			file_num += 1
+			sub_f = open("url_"+str(file_num)+".txt", "w+")
+		line = f.readline()
+	sub_f.close()
+	return file_num
+
+def extract_file_and_parse(file_name, num):
+	f = open(file_name, "r+")
+	w_f = open("yahoo_pd_name_" + num + ".txt", "w+")
+	line = f.readline().rstrip()
+	while(line != ""):
+		try:
+			if "https://tw.buy.yahoo.com" in line:
+				#print (line)
+				page = urllib.request.urlopen(line, timeout = 1)
+				parse_prod_name(page, w_f)
+		except Exception as e:
+			pass
+		line = f.readline().rstrip()
+	f.close()
+	w_f.close()
+
+
+
+def distribute_parse_pn(file_num):
+	num = 1
+	file_list = []
+	num_list = []
+	while(num <= file_num):
+		file_list.append("url_"+str(num)+".txt")
+		num_list.append(num)
+		num += 1
+	num = 1
+	print (file_list)
+	print (num_list)
+	
+	with concurrent.futures.ThreadPoolExecutor(max_workers=len(file_list)) as executor:
+		while(num <= file_num):
+			executor.submit(extract_file_and_parse, file_list[num-1], str(num_list[num-1]))
+			num += 1
+
+
 
 
 if __name__ == '__main__':
-	parse_url_bfs("https://tw.buy.yahoo.com/", 3)
-	#parse_url_bfs("https://tw.buy.yahoo.com/md/AddSupplier1.aspx", 3)
+	#parse_url_bfs("https://tw.buy.yahoo.com/", 3)
+	#file_num = divide_url_all()
+	distribute_parse_pn(5)
 	"""
 	f = open("yahoo_pd_name.txt", "w+")
 	url_n = 0
